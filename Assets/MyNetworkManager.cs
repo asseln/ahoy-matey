af9f6f34353fc233dfce410d5263c940dc1196ed
Assel <asseln86@gmail.com>
2018-11-13 17:44:05 +0000

    Initial commit

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+   
+	
+	public void MyStartHost () {
+        Debug.Log(Time.timeSinceLevelLoad + " starting Host at: ");
+        StartHost();
+
+
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started at: ");
+    }
+
+    public override void OnStartClient(NetworkClient myclient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad+" client start requested: ");
+        InvokeRepeating("PrintDots", 0f, 0f);
+       
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " client is connected to IP: "+conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
+}
